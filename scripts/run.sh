93d9afe380162d2d65a31602693307f83efbd4a7
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-02-12 22:16:19 -0500

    Fix missing environment variable check and add Redis flushall command

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,14 +8,16 @@ export LOG_LEVEL="-4"
 required_env_vars=("AUTH_TOKEN_ISS" "AUTH_TOKEN_AUD")
 
 for var in "${required_env_vars[@]}"; do
-    if [[ -z "${!var}" ]]; then
-        echo "Required environment variable $var is missing"
-        exit 1
-    fi
+  if [[ -z "${!var}" ]]; then
+    echo "Required environment variable $var is missing"
+    exit 1
+  fi
 done
 
 rm ./tmp/main
 
 go build -o ./tmp/main ./cmd/actlabs-hub
+
+redis-cli flushall
 
 export LOG_LEVEL="${LOG_LEVEL}" && export PORT="8883"
