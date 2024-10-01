5715ba3d29bd57fa42cddc68b5bc6c6b31c2996c
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-10-01 15:12:15 -0400

    refactor: Enable shared key access for storage account in setup script and wait for changes to take effect

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -210,12 +210,14 @@ function create_storage_account() {
   SHARED_KEY_ACCESS=$(az storage account show --name ${STORAGE_ACCOUNT_NAME} -g "${RESOURCE_GROUP}" --query "allowSharedKeyAccess" --output tsv)
   if [[ ${SHARED_KEY_ACCESS} == "false" ]]; then
     log "enabling shared key access for storage account ${STORAGE_ACCOUNT_NAME}"
-    az storage account update --name ${STORAGE_ACCOUNT_NAME} -g "${RESOURCE_GROUP}" --allow-shared-key-access true
+    az storage account update --name ${STORAGE_ACCOUNT_NAME} -g "${RESOURCE_GROUP}" --allow-shared-key-access true >/dev/null 2>&1
     if [ $? -ne 0 ]; then
       err "failed to enable shared key access for storage account ${STORAGE_ACCOUNT_NAME}"
       exit 1
     else
       log "shared key access enabled for storage account ${STORAGE_ACCOUNT_NAME}"
+      log "waiting for 30 seconds for the changes to take effect"
+      sleep_with_progress 30
     fi
   else
     log "shared key access is already enabled for storage account ${STORAGE_ACCOUNT_NAME}"
