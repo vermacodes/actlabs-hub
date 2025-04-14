8b72afd73bbe6d462bc4d63524271205f04fe622
hc-github-team-tf-azure <>
2025-04-14 15:51:08 -0400

    feat: Update setup script to manage roles for identities and improve logging

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ACTLABS_SP_APP_ID="00399ddd-434c-4b8a-84be-d096cff4f494"
+
+# If user is in fdpo tenant, then script will replace ACTLABS_SP_APP_ID with ACTLABS_FDPO_SP_APP_ID
 ACTLABS_FDPO_SP_APP_ID="50cc6d33-3224-477f-b2bd-5c1c6595fdf5"
-ACTLABS_MSI_APP_ID="bee16ca1-a401-40ee-bb6a-34349ebd993e"
+ACTLABS_MSI_APP_ID="9735b762-ef8d-477b-af26-13c9b8d6f35c"
 RESOURCE_GROUP="repro-project"
 
 # Add some color
@@ -131,245 +133,87 @@ ensure_user_is_contributor() {
   return 0
 }
 
-# Function to check if a resource group exists
-# If the resource group doesn't exist, create one
-function create_resource_group() {
-  # Check if the resource group exists
-  RG_EXISTS=$(az group exists --name "${RESOURCE_GROUP}" --output tsv)
+# Function to ensure that identity has contributor and user access administrator role on the subscription
+function ensure_required_roles() {
 
-  if [[ "${RG_EXISTS}" == "true" ]]; then
-    log "resource group ${RESOURCE_GROUP} already exists"
-    return 0
+  IDENTITY=$1
+  if [[ -z "${IDENTITY}" ]]; then
+    err "identity is not provided"
+    exit 1
+  fi
+  # Check if the identity is 'Contributor' on the subscription
+  SP_ROLE=$(az role assignment list --assignee "${IDENTITY}" --scope "/subscriptions/${SUBSCRIPTION_ID}" --query "[?roleDefinitionName=='Contributor'].roleDefinitionName" -o tsv)
+  if [[ -n "${SP_ROLE}" ]]; then
+    log "actlabs ID ${IDENTITY} is already 'Contributor' on the subscription"
   else
-    log "creating resource group with name ${RESOURCE_GROUP}"
-
-    # # Ask the user for a location if one wasn't provided
-    # if [[ -z "${LOCATION}" ]]; then
-    #   gap
-    #   LOCATION=$(az account list-locations --query "[?metadata.regionType!='Logical' && metadata.physicalLocation!=null].name" -o tsv)
-    #   echo "Please select a location (azure region) from the list below:"
-    #   select LOCATION in ${LOCATION}; do
-    #     if [[ -n "${LOCATION}" ]]; then
-    #       break
-    #     else
-    #       echo "Invalid selection, please try again"
-    #     fi
-    #   done
-    # fi
-
-    export LOCATION="eastus"
-
-    # Create the resource group
-    az group create --name "${RESOURCE_GROUP}" --location "${LOCATION}"
-    if [ $? -ne 0 ]; then
-      err "failed to create resource group ${RESOURCE_GROUP}"
-      exit 1
-    else
-      log "resource group ${RESOURCE_GROUP} created"
-    fi
+    err "actlabs ID ${IDENTITY} is not 'Contributor' on the subscription. Registration requires 'Owner' access to the subscription. Please ask the Owner to register the subscription with the actlabs"
+    exit 1
   fi
 
-  return 0
+  # Check if the identity is 'User Access Administrator' on the subscription
+  SP_ROLE=$(az role assignment list --assignee "${IDENTITY}" --scope "/subscriptions/${SUBSCRIPTION_ID}" --query "[?roleDefinitionName=='User Access Administrator'].roleDefinitionName" -o tsv)
+  if [[ -n "${SP_ROLE}" ]]; then
+    log "actlabs ID ${IDENTITY} is already 'User Access Administrator' on the subscription"
+  else
+    err "actlabs ID ${IDENTITY} is not 'User Access Administrator' on the subscription. Registration requires 'Owner' access to the subscription. Please ask the Owner to register the subscription with the actlabs"
+    exit 1
+  fi
 }
 
-# Function to check if a storage account exists in the resource group
-# If the storage account doesn't exist, create one with a random name
-function create_storage_account() {
-  # Get a list of all storage accounts in the resource group
-  SA_LIST=$(az storage account list --resource-group "${RESOURCE_GROUP}" --query "[].name" -o tsv)
-
-  for SA in $SA_LIST; do
-    if [[ "${SA}" == "${USER_ALIAS_FOR_SA}"* ]]; then
-      log "storage account already exists with name ${SA}"
-      STORAGE_ACCOUNT_NAME="$SA"
-      break
-    fi
-  done
-
-  if [[ -z "${STORAGE_ACCOUNT_NAME}" ]]; then
-    # Generate a random name for the storage account
-    RANDOM_NAME=$(openssl rand -hex 2)
-    STORAGE_ACCOUNT_NAME="${USER_ALIAS_FOR_SA}sa${RANDOM_NAME}"
-
-    log "creating storage account with name ${STORAGE_ACCOUNT_NAME} in resource group ${RESOURCE_GROUP}"
-
-    # Register the storage provider
-    az provider register --namespace Microsoft.Storage --consent-to-permission --wait
-    # Create the storage account
-    az storage account create --name "${STORAGE_ACCOUNT_NAME}" --resource-group "${RESOURCE_GROUP}" --sku Standard_LRS
-    if [ $? -ne 0 ]; then
-      err "failed to create storage account ${STORAGE_ACCOUNT_NAME}"
-      exit 1
-    else
-      log "storage account ${STORAGE_ACCOUNT_NAME} created"
-    fi
-  fi
-
-  # Enable shared key access to the storage account
-  log "checking if shared key access is enabled for storage account ${STORAGE_ACCOUNT_NAME}"
-  SHARED_KEY_ACCESS=$(az storage account show --name ${STORAGE_ACCOUNT_NAME} -g "${RESOURCE_GROUP}" --query "allowSharedKeyAccess" --output tsv)
-  if [[ ${SHARED_KEY_ACCESS} == "false" ]]; then
-    log "enabling shared key access for storage account ${STORAGE_ACCOUNT_NAME}"
-    az storage account update --name ${STORAGE_ACCOUNT_NAME} -g "${RESOURCE_GROUP}" --allow-shared-key-access true >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-      err "failed to enable shared key access for storage account ${STORAGE_ACCOUNT_NAME}"
-      exit 1
-    else
-      log "shared key access enabled for storage account ${STORAGE_ACCOUNT_NAME}"
-      log "waiting for 30 seconds for the changes to take effect"
-      sleep_with_progress 30
-    fi
-  else
-    log "shared key access is already enabled for storage account ${STORAGE_ACCOUNT_NAME}"
-  fi
-
-  # get storage account key
-  STORAGE_ACCOUNT_KEY=$(az storage account keys list --resource-group "${RESOURCE_GROUP}" --account-name "${STORAGE_ACCOUNT_NAME}" --query "[0].value" -o tsv)
-
-  # check if a blob container named 'tfstate' exists in the storage account
-  # if not create one
-  log "checking if blob container tfstate exists in storage account ${STORAGE_ACCOUNT_NAME}"
-  CONTAINER_EXISTS=$(az storage container exists --name "tfstate" --account-name "${STORAGE_ACCOUNT_NAME}" --account-key "${STORAGE_ACCOUNT_KEY}" --query "exists" -o tsv)
-  if [[ "${CONTAINER_EXISTS}" == "true" ]]; then
-    log "Blob container tfstate already exists in storage account ${STORAGE_ACCOUNT_NAME}"
-  else
-    log "Blob container tfstate does not exist in storage account ${STORAGE_ACCOUNT_NAME}, creating"
-    az storage container create --name "tfstate" --account-name "${STORAGE_ACCOUNT_NAME}"
-    if [ $? -ne 0 ]; then
-      err "Failed to create blob container tfstate in storage account ${STORAGE_ACCOUNT_NAME}"
-      exit 1
-    else
-      log "Blob container tfstate created in storage account ${STORAGE_ACCOUNT_NAME}"
-    fi
-  fi
-
-  # check if a blob container named 'labs' exists in the storage account
-  # if not create one
-  log "checking if blob container labs exists in storage account ${STORAGE_ACCOUNT_NAME}"
-  CONTAINER_EXISTS=$(az storage container exists --name "labs" --account-name "${STORAGE_ACCOUNT_NAME}" --account-key "${STORAGE_ACCOUNT_KEY}" --query "exists" -o tsv)
-  if [[ "${CONTAINER_EXISTS}" == "true" ]]; then
-    log "Blob container labs already exists in storage account ${STORAGE_ACCOUNT_NAME}"
-  else
-    log "Blob container labs does not exist in storage account ${STORAGE_ACCOUNT_NAME}, creating"
-    az storage container create --name "labs" --account-name "${STORAGE_ACCOUNT_NAME}"
-    if [ $? -ne 0 ]; then
-      err "Failed to create blob container labs in storage account ${STORAGE_ACCOUNT_NAME}"
-      exit 1
-    else
-      log "Blob container labs created in storage account ${STORAGE_ACCOUNT_NAME}"
-    fi
-  fi
-
-  return 0
-}
-
-# Function to check if managed identity is 'Contributor' on the subscription
-# If not, assign the managed identity the 'Contributor' role on the subscription
+# Function to check if identity is 'Contributor' on the subscription
+# If not, assign the identity the 'Contributor' role on the subscription
 function assign_contributor_role() {
-  # Check if the managed identity is 'Contributor' on the subscription
-  MI_ROLE=$(az role assignment list --assignee "${ACTLABS_SP_APP_ID}" --scope "/subscriptions/${SUBSCRIPTION_ID}" --query "[?roleDefinitionName=='Contributor'].roleDefinitionName" -o tsv)
 
-  if [[ -n "${MI_ROLE}" ]]; then
-    log "actlabs is already 'Contributor' on the subscription"
+  IDENTITY=$1
+  if [[ -z "${IDENTITY}" ]]; then
+    err "identity is not provided"
+    exit 1
+  fi
+
+  # Check if the identity is 'Contributor' on the subscription
+  SP_ROLE=$(az role assignment list --assignee "${IDENTITY}" --scope "/subscriptions/${SUBSCRIPTION_ID}" --query "[?roleDefinitionName=='Contributor'].roleDefinitionName" -o tsv)
+
+  if [[ -n "${SP_ROLE}" ]]; then
+    log "actlabs ID ${IDENTITY} is already 'Contributor' on the subscription"
   else
-    log "assigning actlabs 'Contributor' role on the subscription"
-    # Assign the managed identity the 'Contributor' role on the subscription
-    az role assignment create --assignee "${ACTLABS_SP_APP_ID}" --role Contributor --scope "/subscriptions/${SUBSCRIPTION_ID}"
+    log "assigning actlabs ID ${IDENTITY} 'Contributor' role on the subscription"
+    # Assign the identity the 'Contributor' role on the subscription
+    az role assignment create --assignee "${IDENTITY}" --role Contributor --scope "/subscriptions/${SUBSCRIPTION_ID}"
     if [ $? -ne 0 ]; then
-      err "failed to assign actlabs 'Contributor' role on the subscription"
+      err "failed to assign actlabs ID ${IDENTITY} 'Contributor' role on the subscription"
       exit 1
     else
-      log "actlabs assigned 'Contributor' role on the subscription"
+      log "actlabs ID ${IDENTITY} assigned 'Contributor' role on the subscription"
     fi
   fi
 
   return 0
 }
 
-# Function to check if managed identity is 'User Access Administrator' on the subscription
-# If not, assign the managed identity the 'User Access Administrator' role on the subscription
+# Function to check if given identity is 'User Access Administrator' on the subscription
+# If not, assign the given identity the 'User Access Administrator' role on the subscription
 function assign_user_access_administrator_role() {
-  # Check if the managed identity is 'User Access Administrator' on the subscription
-  MI_ROLE=$(az role assignment list --assignee "${ACTLABS_SP_APP_ID}" --scope "/subscriptions/${SUBSCRIPTION_ID}" --query "[?roleDefinitionName=='User Access Administrator'].roleDefinitionName" -o tsv)
 
-  if [[ -n "${MI_ROLE}" ]]; then
-    log "actlabs is already 'User Access Administrator' on the subscription"
-  else
-    log "assigning actlabs 'User Access Administrator' role on the subscription"
-    # Assign the managed identity the 'User Access Administrator' role on the subscription
-    az role assignment create --assignee "${ACTLABS_SP_APP_ID}" --role "User Access Administrator" --scope "/subscriptions/${SUBSCRIPTION_ID}"
-    if [ $? -ne 0 ]; then
-      err "failed to assign actlabs 'User Access Administrator' role on the subscription"
-      exit 1
-    else
-      log "actlabs assigned 'User Access Administrator' role on the subscription"
-    fi
+  IDENTITY=$1
+  if [[ -z "${IDENTITY}" ]]; then
+    err "identity is not provided"
+    exit 1
   fi
 
-  return 0
-}
+  # Check if the identity is 'User Access Administrator' on the subscription
+  SP_ROLE=$(az role assignment list --assignee "${IDENTITY}" --scope "/subscriptions/${SUBSCRIPTION_ID}" --query "[?roleDefinitionName=='User Access Administrator'].roleDefinitionName" -o tsv)
 
-# Function to check if managed identity is 'Storage Blob Data Contributor' on the resource group repro-project
-# If not, assign the managed identity the 'Storage Blob Data Contributor' role on the resource group repro-project
-function assign_storage_blob_data_contributor_role() {
-  # Check if the managed identity is 'Storage Blob Data Contributor' on the resource group repro-project
-  MI_ROLE=$(az role assignment list --assignee "${ACTLABS_SP_APP_ID}" --scope "/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}" --query "[?roleDefinitionName=='Storage Blob Data Contributor'].roleDefinitionName" -o tsv)
-
-  if [[ -n "${MI_ROLE}" ]]; then
-    log "actlabs is already 'Storage Blob Data Contributor' on the resource group"
+  if [[ -n "${SP_ROLE}" ]]; then
+    log "actlabs ID ${IDENTITY} is already 'User Access Administrator' on the subscription"
   else
-    log "assigning actlabs 'Storage Blob Data Contributor' role on the resource group"
-    # Assign the managed identity the 'Storage Blob Data Contributor' role on the resource group
-    az role assignment create --assignee "${ACTLABS_SP_APP_ID}" --role "Storage Blob Data Contributor" --scope "/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}"
+    log "assigning actlabs ID ${IDENTITY} 'User Access Administrator' role on the subscription"
+    # Assign the identity the 'User Access Administrator' role on the subscription
+    az role assignment create --assignee "${IDENTITY}" --role "User Access Administrator" --scope "/subscriptions/${SUBSCRIPTION_ID}"
     if [ $? -ne 0 ]; then
-      err "failed to assign actlabs 'Storage Blob Data Contributor' role on the resource group"
+      err "failed to assign actlabs ID ${IDENTITY} 'User Access Administrator' role on the subscription"
       exit 1
     else
-      log "actlabs assigned 'Storage Blob Data Contributor' role on the resource group"
-    fi
-  fi
-
-  return 0
-}
-
-# Function to assign actlabs msi as a contributor on the resource group
-function assign_actlabs_msi_contributor_role() {
-  # Check if actlabs msi is a contributor on the resource group
-  ACTLABS_ROLE=$(az role assignment list --assignee "${ACTLABS_MSI_APP_ID}" --scope "/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}" --query "[?roleDefinitionName=='Contributor'].roleDefinitionName" -o tsv)
-
-  if [[ -n "${ACTLABS_ROLE}" ]]; then
-    log "actlabs msi is already 'Contributor' on the resource group"
-  else
-    log "assigning actlabs msi 'Contributor' role on the resource group"
-    # Assign actlabs msi the 'Contributor' role on the resource group
-    az role assignment create --assignee "${ACTLABS_MSI_APP_ID}" --role Contributor --scope "/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}"
-    if [ $? -ne 0 ]; then
-      err "failed to assign actlabs msi 'Contributor' role on the resource group"
-      exit 1
-    else
-      log "actlabs msi assigned 'Contributor' role on the resource group"
-    fi
-  fi
-
-  return 0
-}
-
-# Function to assign actlabs msi as a reader on the subscription
-function assign_actlabs_msi_reader_role() {
-  # Check if actlabs msi is a reader on the subscription
-  ACTLABS_ROLE=$(az role assignment list --assignee "${ACTLABS_MSI_APP_ID}" --scope "/subscriptions/${SUBSCRIPTION_ID}" --query "[?roleDefinitionName=='Reader'].roleDefinitionName" -o tsv)
-
-  if [[ -n "${ACTLABS_ROLE}" ]]; then
-    log "actlabs msi is already 'Reader' on the subscription"
-  else
-    log "assigning actlabs msi 'Reader' role on the subscription"
-    # Assign actlabs msi the 'Reader' role on the subscription
-    az role assignment create --assignee "${ACTLABS_MSI_APP_ID}" --role Reader --scope "/subscriptions/${SUBSCRIPTION_ID}"
-    if [ $? -ne 0 ]; then
-      err "failed to assign actlabs msi 'Reader' role on the subscription"
-      exit 1
-    else
-      log "actlabs msi assigned 'Reader' role on the subscription"
+      log "actlabs ID ${IDENTITY} assigned 'User Access Administrator' role on the subscription"
     fi
   fi
 
@@ -401,8 +245,7 @@ function register_subscription() {
 
   if [ "${HTTP_STATUS}" -ne 200 ]; then
     err "deployment completed, but, failed to automatically register subscription with the lab"
-    ok "Next steps: Go back to UI and register your subscription with the lab"
-    ok "Your subscription id is: ${SUBSCRIPTION_ID}"
+    log "Next steps: Try re-running the script again. If error persists, please contact support using Help & Feedback option on portal and share your subscription id ${SUBSCRIPTION_ID}"
     exit 0
   else
     log "Output: $OUTPUT"
@@ -416,16 +259,14 @@ get_subscription_id
 ensure_user_is_owner
 if [[ "${is_owner}" == false ]]; then
   ensure_user_is_contributor
+  ensure_required_roles "${ACTLABS_MSI_APP_ID}"
+  ensure_required_roles "${ACTLABS_SP_APP_ID}"
 fi
-create_resource_group
-create_storage_account
 if [[ "${is_owner}" == true ]]; then
-  assign_contributor_role
-  assign_user_access_administrator_role
-  assign_storage_blob_data_contributor_role
-  if [[ "${is_fdpo}" == false ]]; then
-    assign_actlabs_msi_contributor_role
-    assign_actlabs_msi_reader_role
-  fi
+  assign_contributor_role "${ACTLABS_SP_APP_ID}"
+  assign_contributor_role "${ACTLABS_MSI_APP_ID}"
+  assign_user_access_administrator_role "${ACTLABS_SP_APP_ID}"
+  assign_user_access_administrator_role "${ACTLABS_MSI_APP_ID}"
+
 fi
 register_subscription
