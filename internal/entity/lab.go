a4d95f7aa79a45ec311702ff2dca0f8b56d48447
vermacodes <19263858+vermacodes@users.noreply.github.com>
2026-04-06 20:12:50 +0000

    feat: implement labentity module with shared types and re-export for compatibility

--- internal/entity/lab.go
+++ internal/entity/lab.go
@@ -1,119 +1,36 @@
 package entity
 
 import (
+	"actlabs/labentity"
 	"context"
 	"io"
 	"mime/multipart"
 )
 
-type TfvarResourceGroupType struct {
-	Location string `json:"location"`
-}
+// Re-export shared types for backward compatibility within hub
+type TfvarResourceGroupType = labentity.TfvarResourceGroupType
+type TfvarDefaultNodePoolType = labentity.TfvarDefaultNodePoolType
+type TfvarServiceMeshType = labentity.TfvarServiceMeshType
+type TfvarAddonsType = labentity.TfvarAddonsType
+type TfvarKubernetesClusterType = labentity.TfvarKubernetesClusterType
+type TfvarAroClusterType = labentity.TfvarAroClusterType
+type TfvarVirtualNetworkType = labentity.TfvarVirtualNetworkType
+type TfvarSubnetType = labentity.TfvarSubnetType
+type TfvarNetworkSecurityGroupType = labentity.TfvarNetworkSecurityGroupType
+type TfvarJumpserverType = labentity.TfvarJumpserverType
+type TfvarFirewallType = labentity.TfvarFirewallType
+type ContainerRegistryType = labentity.ContainerRegistryType
+type AppGatewayType = labentity.AppGatewayType
+type TfvarConfigType = labentity.TfvarConfigType
+type Blob = labentity.Blob
+type LabType = labentity.LabType
 
-type TfvarDefaultNodePoolType struct {
-	EnableAutoScaling         bool   `json:"enableAutoScaling"`
-	MinCount                  int    `json:"minCount"`
-	MaxCount                  int    `json:"maxCount"`
-	VmSize                    string `json:"vmSize"`
-	OnlyCriticalAddonsEnabled bool   `json:"onlyCriticalAddonsEnabled"`
-	OsSku                     string `json:"osSku"`
-}
-
-type TfvarAddonsType struct {
-	AppGateway        bool `json:"appGateway"`
-	MicrosoftDefender bool `json:"microsoftDefender"`
-}
-
-type TfvarKubernetesClusterType struct {
-	KubernetesVersion     string                   `json:"kubernetesVersion"`
-	NetworkPlugin         string                   `json:"networkPlugin"`
-	NetworkPolicy         string                   `json:"networkPolicy"`
-	NetworkPluginMode     string                   `json:"networkPluginMode"`
-	OutboundType          string                   `json:"outboundType"`
-	PrivateClusterEnabled string                   `json:"privateClusterEnabled"`
-	Addons                TfvarAddonsType          `json:"addons"`
-	DefaultNodePool       TfvarDefaultNodePoolType `json:"defaultNodePool"`
-}
-
-type TfvarAroClusterType struct {
-	Version string `json:"version"`
-}
-
-type TfvarVirtualNetworkType struct {
-	AddressSpace []string
-}
-
-type TfvarSubnetType struct {
-	Name            string
-	AddressPrefixes []string
-}
-
-type TfvarNetworkSecurityGroupType struct{}
-
-type TfvarJumpserverType struct {
-	AdminPassword string `json:"adminPassword"`
-	AdminUserName string `json:"adminUsername"`
-}
-
-type TfvarFirewallType struct {
-	SkuName string `json:"skuName"`
-	SkuTier string `json:"skuTier"`
-}
-
-type ContainerRegistryType struct{}
-
-type AppGatewayType struct{}
-
-type TfvarConfigType struct {
-	ResourceGroup         TfvarResourceGroupType          `json:"resourceGroup"`
-	VirtualNetworks       []TfvarVirtualNetworkType       `json:"virtualNetworks"`
-	Subnets               []TfvarSubnetType               `json:"subnets"`
-	Jumpservers           []TfvarJumpserverType           `json:"jumpservers"`
-	NetworkSecurityGroups []TfvarNetworkSecurityGroupType `json:"networkSecurityGroups"`
-	KubernetesClusters    []TfvarKubernetesClusterType    `json:"kubernetesClusters"`
-	AroClusters           []TfvarAroClusterType           `json:"aroClusters"`
-	Firewalls             []TfvarFirewallType             `json:"firewalls"`
-	ContainerRegistries   []ContainerRegistryType         `json:"containerRegistries"`
-	AppGateways           []AppGatewayType                `json:"appGateways"`
-}
-
-// didn't find exported equivalent of https://pkg.go.dev/github.com/Azure/azure-sdk-for-go/sdk/storage/azblob@v1.2.1/internal/generated#BlobItem
-type Blob struct {
-	Name             string `xml:"Name"             json:"name"`
-	VersionId        string `xml:"VersionId"        json:"versionId"`
-	IsCurrentVersion bool   `xml:"IsCurrentVersion" json:"isCurrentVersion"`
-	// Url  string `xml:"Url" json:"url"`
-}
-
+// Re-export lab categories
 var (
-	PrivateLab    = []string{"privatelab", "challengelab"}
-	PublicLab     = []string{"publiclab"}
-	ProtectedLabs = []string{"readinesslab", "mockcase"}
+	PrivateLab    = labentity.PrivateLab
+	PublicLab     = labentity.PublicLab
+	ProtectedLabs = labentity.ProtectedLabs
 )
-
-type LabType struct {
-	Id                       string          `json:"id"`
-	Name                     string          `json:"name"`
-	Description              string          `json:"description"`
-	Tags                     []string        `json:"tags"`
-	Template                 TfvarConfigType `json:"template"`
-	ExtendScript             string          `json:"extendScript"`
-	Message                  string          `json:"message"`
-	Category                 string          `json:"category"`
-	Type                     string          `json:"type"`
-	CreatedBy                string          `json:"createdBy"`
-	CreatedOn                string          `json:"createdOn"`
-	UpdatedBy                string          `json:"updatedBy"`
-	UpdatedOn                string          `json:"updatedOn"`
-	Owners                   []string        `json:"owners"`
-	Editors                  []string        `json:"editors"`
-	Viewers                  []string        `json:"viewers"`
-	RbacEnforcedProtectedLab bool            `json:"rbacEnforcedProtectedLab"`
-	IsPublished              bool            `json:"isPublished"`
-	VersionId                string          `json:"versionId"`
-	IsCurrentVersion         bool            `json:"isCurrentVersion"`
-	SupportingDocumentId     string          `json:"supportingDocumentId"`
-}
 
 type LabService interface {
 	// Private Labs
